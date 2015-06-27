efae3fa5a3915948370b43c2c5133de2c1851c35
Anton Maniskevich <fenixhomepro@gmail.com>
2015-06-27 20:04:54 +0300

    get build version

--- apps_v2/src/main/java/com/ekreative/apps_v2/apps_v2/AppsV2Builder.java
+++ apps_v2/src/main/java/com/ekreative/apps_v2/apps_v2/AppsV2Builder.java
@@ -37,13 +37,16 @@ import javax.servlet.ServletException;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class AppsV2Builder extends Builder {
 
+	private static final String VERSION_NAME = "versionName \"";
 	private final String projectUrl;
 	private final String buildPath;
 
@@ -62,23 +65,14 @@ public class AppsV2Builder extends Builder {
 	}
 
 	@Override
-	public boolean perform(AbstractBuild build, Launcher launcher,
-			BuildListener listener) {
-
-		// This is where you 'build' the project.
-		// Since this is a dummy, we just say 'hello world' and call that a
-		// build.
-
-		// This also shows how you can consult the global configuration of the
-		// builder
-		// if (getDescriptor().getUseFrench())
-		listener.getLogger().println(
-				"Bonjour, user!" + projectUrl + " " + buildPath);
-		// else
-		// listener.getLogger().println("Hello, " + name + "!");
-
+	public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+		listener.getLogger().println("Launch APPS-V2 plugin #ekreativehackday15");
+		listener.getLogger().println("Project Url=" + projectUrl);
+		listener.getLogger().println("Project Path=" + buildPath);
+		String pathToFile = buildPath + "\\build\\outputs\\apk";
+		listener.getLogger().println("Path to build file=" + pathToFile);
 		try {
-			File buildFile = findBuildFile(buildPath, listener.getLogger());
+			File buildFile = findBuildFile(pathToFile, listener.getLogger());
 			if (buildFile != null) {
 				return sendBuild(buildFile, projectUrl,  listener.getLogger());
 			}
@@ -97,7 +91,7 @@ public class AppsV2Builder extends Builder {
 				return Long.valueOf(f1.lastModified()).compareTo(f2.lastModified());
 			}
 		});
-		//TODO debug
+		//just show files
 		for (int i = 0; i < files.length; i++) {
 			logger.println(files[i]);
 		}
@@ -106,15 +100,31 @@ public class AppsV2Builder extends Builder {
 		}
 		return null;
 	}
+	
+	public String getAppVersion(String buildPath, PrintStream logger) {
+		File gradleFile = new File(buildPath+"\\build.gradle");
+		try {
+			String content = new Scanner(gradleFile).useDelimiter("\\Z").next();
+			int startVesion = content.indexOf(VERSION_NAME);
+			if (startVesion != -1) {
+				String substing = content.substring(startVesion+VERSION_NAME.length());
+				return substing.substring(0, substing.indexOf("\""));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return "unknown";
+	}
 
-	private boolean sendBuild(File file, String projectUrl, PrintStream logger)
-			throws IOException, ClientProtocolException {
+	private boolean sendBuild(File file, String projectUrl, PrintStream logger) throws IOException, ClientProtocolException {
 		HttpClient httpclient = new DefaultHttpClient();
+		String appVersion = getAppVersion(buildPath, logger);
+		logger.println("appVersion="+appVersion);
 		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 		FileBody fileBody = new FileBody(file);
 		builder.addPart("app", fileBody);
-		builder.addPart("version", new StringBody("1.0.0",
-				ContentType.TEXT_PLAIN));
+		builder.addPart("name", new StringBody("debug", ContentType.TEXT_PLAIN));
+		builder.addPart("version", new StringBody(appVersion, ContentType.TEXT_PLAIN));
 
 		Header header = new BasicHeader("X-API-Key","5a61db3dcec0fe4616d08084102dad9b6511c1b1");
 		HttpPost post = new HttpPost(projectUrl);
@@ -127,6 +137,7 @@ public class AppsV2Builder extends Builder {
 			logger.println("Successful! Build uploaded!");
 			return true;
 		} else {
+			logger.println("Build upload failde!");
 			HttpEntity entity = response.getEntity();
 			if (entity != null) {
 				logger.println(EntityUtils.toString(entity));
@@ -148,133 +159,4 @@ public class AppsV2Builder extends Builder {
 			return "Upload test build to apps-v2";
 		}
 	}
-	// private final String name;
-	//
-	// // Fields in config.jelly must match the parameter names in the
-	// "DataBoundConstructor"
-	// @DataBoundConstructor
-	// public HelloWorldBuilder(String name) {
-	// this.name = name;
-	// }
-	//
-	// /**
-	// * We'll use this from the <tt>config.jelly</tt>.
-	// */
-	// public String getName() {
-	// return name;
-	// }
-	//
-	// @Override
-	// public boolean perform(AbstractBuild build, Launcher launcher,
-	// BuildListener listener) {
-	// // This is where you 'build' the project.
-	// // Since this is a dummy, we just say 'hello world' and call that a
-	// build.
-	//
-	// // This also shows how you can consult the global configuration of the
-	// builder
-	// if (getDescriptor().getUseFrench())
-	// listener.getLogger().println("Bonjour, "+name+"!");
-	// else
-	// listener.getLogger().println("Hello, "+name+"!");
-	// return true;
-	// }
-	//
-	// // Overridden for better type safety.
-	// // If your plugin doesn't really define any property on Descriptor,
-	// // you don't have to do this.
-	// @Override
-	// public DescriptorImpl getDescriptor() {
-	// return (DescriptorImpl)super.getDescriptor();
-	// }
-	//
-	// /**
-	// * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
-	// * The class is marked as public so that it can be accessed from views.
-	// *
-	// * <p>
-	// * See
-	// <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-	// * for the actual HTML fragment for the configuration screen.
-	// */
-	// @Extension // This indicates to Jenkins that this is an implementation of
-	// an extension point.
-	// public static final class DescriptorImpl extends
-	// BuildStepDescriptor<Builder> {
-	// /**
-	// * To persist global configuration information,
-	// * simply store it in a field and call save().
-	// *
-	// * <p>
-	// * If you don't want fields to be persisted, use <tt>transient</tt>.
-	// */
-	// private boolean useFrench;
-	//
-	// /**
-	// * In order to load the persisted global configuration, you have to
-	// * call load() in the constructor.
-	// */
-	// public DescriptorImpl() {
-	// load();
-	// }
-	//
-	// /**
-	// * Performs on-the-fly validation of the form field 'name'.
-	// *
-	// * @param value
-	// * This parameter receives the value that the user has typed.
-	// * @return
-	// * Indicates the outcome of the validation. This is sent to the browser.
-	// * <p>
-	// * Note that returning {@link FormValidation#error(String)} does not
-	// * prevent the form from being saved. It just means that a message
-	// * will be displayed to the user.
-	// */
-	// public FormValidation doCheckName(@QueryParameter String value)
-	// throws IOException, ServletException {
-	// if (value.length() == 0)
-	// return FormValidation.error("Please set a name");
-	// if (value.length() < 4)
-	// return FormValidation.warning("Isn't the name too short?");
-	// return FormValidation.ok();
-	// }
-	//
-	// public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-	// // Indicates that this builder can be used with all kinds of project
-	// types
-	// return true;
-	// }
-	//
-	// /**
-	// * This human readable name is used in the configuration screen.
-	// */
-	// public String getDisplayName() {
-	// return "Say hello world";
-	// }
-	//
-	// @Override
-	// public boolean configure(StaplerRequest req, JSONObject formData) throws
-	// FormException {
-	// // To persist global configuration information,
-	// // set that to properties and call save().
-	// useFrench = formData.getBoolean("useFrench");
-	// // ^Can also use req.bindJSON(this, formData);
-	// // (easier when there are many fields; need set* methods for this, like
-	// setUseFrench)
-	// save();
-	// return super.configure(req,formData);
-	// }
-	//
-	// /**
-	// * This method returns true if the global configuration says we should
-	// speak French.
-	// *
-	// * The method name is bit awkward because global.jelly calls this method
-	// to determine
-	// * the initial state of the checkbox by the naming convention.
-	// */
-	// public boolean getUseFrench() {
-	// return useFrench;
-	// }
-	// }
 }
